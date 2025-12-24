de667dc078fd06ea42d56c6d2269e8f9abbce3bf
HIES <mail.rs.dev@gmail.com>
2025-12-24 08:51:54 +0530

    Search text box has been optimised for performance

--- SearchTextBox.xaml.cs
+++ SearchTextBox.xaml.cs
@@ -14,15 +14,9 @@ namespace RS.WPF.SearchBox;
 /// </summary>
 public partial class SearchTextBox : UserControl,INotifyPropertyChanged
 {
-    public static readonly DependencyProperty HintTextProperty =
-        DependencyProperty.Register("HintText", typeof(string), typeof(SearchTextBox),
-            new PropertyMetadata(""));
-    public static readonly DependencyProperty TextValueProperty =
-        DependencyProperty.Register("TextValue", typeof(string), typeof(SearchTextBox),
-            new PropertyMetadata(""));
-    public static readonly DependencyProperty TextBoxTypeProperty =
-       DependencyProperty.Register("TextBoxType", typeof(SearchTextBoxType), typeof(SearchTextBox),
-           new PropertyMetadata(SearchTextBoxType.AlphaNumeric));
+    public static readonly DependencyProperty HintTextProperty =DependencyProperty.Register("HintText", typeof(string), typeof(SearchTextBox),new PropertyMetadata(""));
+    public static readonly DependencyProperty TextValueProperty =DependencyProperty.Register("TextValue", typeof(string), typeof(SearchTextBox),new PropertyMetadata(""));
+    public static readonly DependencyProperty TextBoxTypeProperty =DependencyProperty.Register("TextBoxType", typeof(SearchTextBoxType), typeof(SearchTextBox),new PropertyMetadata(SearchTextBoxType.AlphaNumeric));
     public enum SearchTextBoxType
     {
         AlphaNumeric = 0,
@@ -34,13 +28,15 @@ public partial class SearchTextBox : UserControl,INotifyPropertyChanged
     }
     public event PropertyChangedEventHandler PropertyChanged;
     public event EventHandler TextChanged;
+    private static readonly Regex FloatRegex = new Regex("^[.][0-9]+$|^[0-9]*[.]{0,1}[0-9]*$", RegexOptions.Compiled);
+    private static readonly Regex NumberRegex = new Regex("[^0-9]+", RegexOptions.Compiled);
 
     public SearchTextBox()
     {
         InitializeComponent();
 
-        this.GrdSearchBox.DataContext = this;
-
+        this.SearchInput.GotFocus += (s, e) => this.ContainerBorder.Background = (Brush)Resources["FocusBackgroundBrush"];
+        this.SearchInput.LostFocus += (s, e) => this.ContainerBorder.Background = (Brush)Resources["DefaultBackgroundBrush"];
     }
     public string HintText
     {
@@ -54,32 +50,17 @@ public partial class SearchTextBox : UserControl,INotifyPropertyChanged
     }
     public string TextValue
     {
-        get { return this.txtSearch.Text.ToUpper() == this.HintText.ToUpper() ? "" : this.txtSearch.Text.Trim(); }
-        set { SetValue(TextValueProperty, value); }
+        get => (string)GetValue(TextValueProperty);
+        set => SetValue(TextValueProperty, value);
     }
-    public int IntValue
-    {
-        get { int.TryParse(TextValue, out int xInt); return xInt; }
-    }
-    public long LongValue
-    {
-        get { long.TryParse(TextValue, out long xLong); return xLong; }
-    }
-    public float FloatValue
-    {
-        get { float.TryParse(TextValue, out float xFloat); return xFloat; }
-    }
-    public decimal DecimalValue
-    {
-        get { decimal.TryParse(TextValue, out decimal xDecimal); return xDecimal; }
-    }
-    public DateTime DateValue
-    {
-        get { DateTime.TryParse(TextValue, out DateTime xDate); return xDate; }
-    }
+    public int IntValue => int.TryParse(TextValue, out int x) ? x : 0;
+    public long LongValue => long.TryParse(TextValue, out long x) ? x : 0;
+    public float FloatValue => float.TryParse(TextValue, out float x) ? x : 0;
+    public decimal DecimalValue => decimal.TryParse(TextValue, out decimal x) ? x : 0;
+    public DateTime DateValue => DateTime.TryParse(TextValue, out DateTime x) ? x : DateTime.MinValue;
     public void SelectAll()
     {
-        txtSearch.SelectAll();
+        SearchInput.SelectAll();
     }
 
     #region EVENTS
@@ -87,81 +68,28 @@ public partial class SearchTextBox : UserControl,INotifyPropertyChanged
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
-    private void UserControl_Loaded(object sender, RoutedEventArgs e)
+    private void BtnClear_Click(object sender, RoutedEventArgs e)
     {
-        if (!txtSearch.IsFocused)
-        {
-            if (string.IsNullOrWhiteSpace(this.txtSearch.Text))
-            {
-                this.txtSearch.Text = this.HintText;
-            }
-        }
+        this.TextValue = string.Empty;
+        this.SearchInput.Focus();
     }
-    private void TxtSearch_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        if (this.txtSearch.Text.ToUpper() == this.HintText.ToUpper())
-        {
-            this.txtSearch.Foreground = Brushes.Gray;
-        }
-        else
-        {
-            this.txtSearch.Foreground = Brushes.Black;
-        }
-        TextChanged?.Invoke(this, EventArgs.Empty);
-    }
-    private void TxtSearch_GotFocus(object sender, RoutedEventArgs e)
-    {
-        if (this.txtSearch.Text.ToUpper() == this.HintText.ToUpper()) 
-        { 
-            this.txtSearch.Text = ""; 
-        }
-        else
-        {
-            this.txtSearch.SelectAll();
-        }
-        this.BrderSearchBox.Background = Brushes.LightGoldenrodYellow;
-    }
-    private void txtSearch_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
-    {
-        if (this.txtSearch.Text.ToUpper() == this.HintText.ToUpper())
-        {
-            this.txtSearch.Text = "";
-        }
-        else
-        {
-            this.txtSearch.SelectAll();
-        }
-        this.BrderSearchBox.Background = Brushes.LightGoldenrodYellow;
-    }
-    private void TxtSearch_LostFocus(object sender, RoutedEventArgs e)
-    {
-        this.BrderSearchBox.Background = Brushes.White;
-        if (string.IsNullOrWhiteSpace(this.txtSearch.Text))
-        {
-            this.txtSearch.Text = this.HintText;
-        }
-    }
-    private void txtSearch_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
-    {
-        this.BrderSearchBox.Background = Brushes.White;
-        if (string.IsNullOrWhiteSpace(this.txtSearch.Text))
-        {
-            this.txtSearch.Text = this.HintText;
-        }
-    }
-    private void TxtSearch_PreviewTextInput(object sender, TextCompositionEventArgs e)
+
+    private void SearchInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
     {
         if (this.TextBoxType == SearchTextBoxType.FloatValue)
         {
-            Regex regex = new Regex("^[.][0-9]+$|^[0-9]*[.]{0,1}[0-9]*$");
-            e.Handled = !regex.IsMatch(txtSearch.Text.Insert(txtSearch.SelectionStart, e.Text));
+            e.Handled = !FloatRegex.IsMatch(SearchInput.Text.Insert(SearchInput.SelectionStart, e.Text));
         }
-        else if (this.TextBoxType == SearchTextBoxType.OnlyNumbers || this.TextBoxType == SearchTextBoxType.IntegerValue ||
-            this.TextBoxType == SearchTextBoxType.LongValue)
+        else if (this.TextBoxType == SearchTextBoxType.OnlyNumbers ||
+                 this.TextBoxType == SearchTextBoxType.IntegerValue ||
+                 this.TextBoxType == SearchTextBoxType.LongValue)
         {
-            Regex regex = new Regex("[^0-9]+");
-            e.Handled = regex.IsMatch(e.Text);
+            e.Handled = NumberRegex.IsMatch(e.Text);
         }
+    }
+    private void SearchInput_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        TextChanged?.Invoke(this, EventArgs.Empty);
     }
     #endregion
 }
